1f5474614ad321d524cda71361003dc7ef73f972
Marvin Rösch <marvinroesch99@gmail.com>
2021-12-08 18:13:55 +0100

    Remove superfluous prop quotes in deploy script

--- src/deploy.ts
+++ src/deploy.ts
@@ -9,137 +9,137 @@ if (token === undefined) {
 
 const cmds: ApplicationCommandDataResolvable[] = [
   {
-    'type': 'CHAT_INPUT',
-    'name': 'spoiler-attachments',
-    'description': 'Send spoilered attachments to this channel from any platform!',
-    'options': [
+    type: 'CHAT_INPUT',
+    name: 'spoiler-attachments',
+    description: 'Send spoilered attachments to this channel from any platform!',
+    options: [
       {
-        'type': 'STRING',
-        'name': 'caption',
-        'description': 'A caption to add to your attachments',
-        'required': false
+        type: 'STRING',
+        name: 'caption',
+        description: 'A caption to add to your attachments',
+        required: false
       }
     ],
-    'defaultPermission': true
+    defaultPermission: true
   },
   {
-    'type': 'CHAT_INPUT',
-    'name': 'auto-publish',
-    'description': 'Manage for which channels messages are automatically published',
-    'options': [
+    type: 'CHAT_INPUT',
+    name: 'auto-publish',
+    description: 'Manage for which channels messages are automatically published',
+    options: [
       {
-        'type': 'SUB_COMMAND',
-        'name': 'start',
-        'description': 'Start auto-publishing future messages in a channel',
-        'options': [
+        type: 'SUB_COMMAND',
+        name: 'start',
+        description: 'Start auto-publishing future messages in a channel',
+        options: [
           {
-            'type': 'CHANNEL',
-            'name': 'channel',
-            'description': 'Channel to start watching',
-            'required': true
+            type: 'CHANNEL',
+            name: 'channel',
+            description: 'Channel to start watching',
+            required: true
           }
         ]
       },
       {
-        'type': 'SUB_COMMAND',
-        'name': 'stop',
-        'description': 'Stop auto-publishing future messages in a channel',
-        'options': [
+        type: 'SUB_COMMAND',
+        name: 'stop',
+        description: 'Stop auto-publishing future messages in a channel',
+        options: [
           {
-            'type': 'CHANNEL',
-            'name': 'channel',
-            'description': 'Channel to stop watching',
-            'required': true
+            type: 'CHANNEL',
+            name: 'channel',
+            description: 'Channel to stop watching',
+            required: true
           }
         ]
       },
       {
-        'type': 'SUB_COMMAND',
-        'name': 'list',
-        'description': 'List all channels that are currently auto-published'
+        type: 'SUB_COMMAND',
+        name: 'list',
+        description: 'List all channels that are currently auto-published'
       }
     ],
-    'defaultPermission': false
+    defaultPermission: false
   },
   {
-    'type': 'CHAT_INPUT',
-    'name': 'auto-thread-invite',
-    'description': 'Manage for which channels and roles members are automatically invited to new threads',
-    'options': [
+    type: 'CHAT_INPUT',
+    name: 'auto-thread-invite',
+    description: 'Manage for which channels and roles members are automatically invited to new threads',
+    options: [
       {
-        'type': 'SUB_COMMAND',
-        'name': 'start',
-        'description': 'Start auto-inviting role members to new threads in a channel',
-        'options': [
+        type: 'SUB_COMMAND',
+        name: 'start',
+        description: 'Start auto-inviting role members to new threads in a channel',
+        options: [
           {
-            'type': 'CHANNEL',
-            'name': 'channel',
-            'description': 'Channel to start watching',
-            'required': true
+            type: 'CHANNEL',
+            name: 'channel',
+            description: 'Channel to start watching',
+            required: true
           },
           {
-            'type': 'ROLE',
-            'name': 'role',
-            'description': 'Role whose members to invite to new threads',
-            'required': true
+            type: 'ROLE',
+            name: 'role',
+            description: 'Role whose members to invite to new threads',
+            required: true
           }
         ]
       },
       {
-        'type': 'SUB_COMMAND',
-        'name': 'stop',
-        'description': 'Stop auto-inviting role members to new threads in a channel',
-        'options': [
+        type: 'SUB_COMMAND',
+        name: 'stop',
+        description: 'Stop auto-inviting role members to new threads in a channel',
+        options: [
           {
-            'type': 'CHANNEL',
-            'name': 'channel',
-            'description': 'Channel to stop watching',
-            'required': true
+            type: 'CHANNEL',
+            name: 'channel',
+            description: 'Channel to stop watching',
+            required: true
           },
           {
-            'type': 'ROLE',
-            'name': 'role',
-            'description': 'Role whose members to no longer invite to new threads',
-            'required': true
+            type: 'ROLE',
+            name: 'role',
+            description: 'Role whose members to no longer invite to new threads',
+            required: true
           }
         ]
       },
       {
-        'type': 'SUB_COMMAND',
-        'name': 'list',
-        'description': 'List all channels that are currently watched for new threads',
-        'options': [
+        type: 'SUB_COMMAND',
+        name: 'list',
+        description: 'List all channels that are currently watched for new threads',
+        options: [
           {
-            'type': 'CHANNEL',
-            'name': 'channel',
-            'description': 'Channel to get automatically invited roles for',
-            'required': false
+            type: 'CHANNEL',
+            name: 'channel',
+            description: 'Channel to get automatically invited roles for',
+            required: false
           }
         ]
       }
     ],
-    'defaultPermission': false
+    defaultPermission: false
   },
   {
-    'type': 'CHAT_INPUT',
-    'name': 'raw',
-    'description': 'Get the raw contents of a message',
-    'options': [
+    type: 'CHAT_INPUT',
+    name: 'raw',
+    description: 'Get the raw contents of a message',
+    options: [
       {
-        'type': 'STRING',
-        'name': 'message',
-        'description': 'Link to a message',
-        'required': true
+        type: 'STRING',
+        name: 'message',
+        description: 'Link to a message',
+        required: true
       }
     ],
-    'defaultPermission': true
+    defaultPermission: true
   }
 ];
-
 
 const client = new Client({ intents: [] });
 client.once('ready', async () => {
   await client.application.commands.set(cmds);
+  console.log('All commands deployed');
   client.destroy();
 });
 
